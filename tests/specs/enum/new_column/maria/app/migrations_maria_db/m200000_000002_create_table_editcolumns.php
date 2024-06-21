72b927b270822ecbc161ee5bbe6e2d8b9243c782
Sohel Ahmed Mesaniya <sam.wankaner@gmail.com>
2024-06-21 20:06:39 +0530

    Fix another failing tests - EnumTest::testAddNewColumn

--- tests/specs/enum/new_column/maria/app/migrations_maria_db/m200000_000002_create_table_editcolumns.php
+++ tests/specs/enum/new_column/maria/app/migrations_maria_db/m200000_000002_create_table_editcolumns.php
@@ -3,13 +3,13 @@
 /**
  * Table for Editcolumn
  */
-class m200000_000000_create_table_editcolumns extends \yii\db\Migration
+class m200000_000002_create_table_editcolumns extends \yii\db\Migration
 {
     public function up()
     {
         $this->createTable('{{%editcolumns}}', [
             'id' => $this->primaryKey(),
-            'device' => $this->text()->null(),
+            'device' => $this->text()->null()->defaultValue(null),
             'connection' => 'enum("WIRED", "WIRELESS") NOT NULL DEFAULT \'WIRED\'',
             'camelCaseCol' => 'enum("ONE", "TWO", "THREE") NOT NULL DEFAULT \'TWO\'',
         ]);
