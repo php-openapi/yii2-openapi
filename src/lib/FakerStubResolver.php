47e5e844f0afccd0e50094153a10980f1ccdbbe9
Sohel Ahmed Mesaniya <sam.wankaner@gmail.com>
2025-03-12 18:15:29 +0530

    Revert "Example usage for https://github.com/php-openapi/yii2-openapi/pull/98#issuecomment-2717759279"
    This reverts commit 95380350d6f6df56d8a99794f03ecf5aa95e6361.

--- src/lib/FakerStubResolver.php
+++ src/lib/FakerStubResolver.php
@@ -16,7 +16,6 @@ use cebe\openapi\exceptions\UnresolvableReferenceException;
 use cebe\openapi\ReferenceContext;
 use cebe\openapi\spec\Reference;
 use cebe\openapi\spec\Schema;
-use cebe\openapi\SpecBaseObject;
 use cebe\openapi\SpecObjectInterface;
 use cebe\yii2openapi\lib\exceptions\InvalidDefinitionException;
 use cebe\yii2openapi\lib\items\Attribute;
@@ -330,9 +329,7 @@ class FakerStubResolver
             if (!empty($prop->properties)) { // nested object
                 $result = $this->{__FUNCTION__}($prop);
             } else {
-//                ($prop instanceof SpecBaseObject) && var_dump($prop->rawSpec);
-//                $result = $this->aElementFaker(['items' => $prop->getSerializableData()], $name);
-                $result = $this->aElementFaker(['items' => $prop->rawSpec], $name);
+                $result = $this->aElementFaker(['items' => $prop->getSerializableData()], $name);
             }
             $props .= '\'' . $name . '\' => ' . $result . ',' . PHP_EOL;
         }
@@ -361,7 +358,7 @@ class FakerStubResolver
         foreach ($items->oneOf as $key => $aDataType) {
             /** @var Schema|Reference $aDataType */
 
-            $inp = $aDataType instanceof Reference ? $aDataType : ['items' => $aDataType->rawSpec];
+            $inp = $aDataType instanceof Reference ? $aDataType : ['items' => $aDataType->getSerializableData()];
             $aFaker = $this->aElementFaker($inp, $this->attribute->columnName);
             $result .= '$dataType' . $key . ' = ' . $aFaker . ';';
         }
@@ -406,7 +403,7 @@ class FakerStubResolver
             return '(new ' . $class . ')->generateModel()->attributes';
         }
 
-        $inp = $data instanceof SpecObjectInterface ? $data->rawSpec : $data;
+        $inp = $data instanceof SpecObjectInterface ? $data->getSerializableData() : $data;
         $aElementData = Json::decode(Json::encode($inp));
         $columnName = $columnName ?? 'unnamedProp';
         $compoSchemaData = [
