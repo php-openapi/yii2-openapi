963c7e29e1dfe34c7e998ae50b8bbf47696cfec7
Sohel Ahmed Mesaniya <sam.wankaner@gmail.com>
2025-03-28 15:25:52 +0530

    Implement Module.php file generation for `urlPrefixes.module` config + fix issues + fix failing tests + refactor + complete the test

---
+++ tests/specs/issue_fix/14_module_config_in_url_prefixes/mysql/greet/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\greet;
+
+class DefaultController extends \app\greet\base\DefaultController
+{
+
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        //TODO implement checkAccess
+    }
+
+    public function action()
+    {
+        //TODO implement action
+    }
+
+
+}
+
--- tests/unit/issues/Issue14Test.php
+++ tests/unit/issues/Issue14Test.php
@@ -26,12 +26,12 @@ class Issue14Test extends DbTestCase
     {
         $testFile = Yii::getAlias("@specs/issue_fix/14_module_config_in_url_prefixes/index.php");
         $this->runGenerator($testFile);
-//        $actualFiles = FileHelper::findFiles(Yii::getAlias('@app'), [
-//            'recursive' => true,
-//        ]);
-//        $expectedFiles = FileHelper::findFiles(Yii::getAlias("@specs/issue_fix/14_nested_module_in_x_route/mysql"), [
-//            'recursive' => true,
-//        ]);
-//        $this->checkFiles($actualFiles, $expectedFiles);
+        $actualFiles = FileHelper::findFiles(Yii::getAlias('@app'), [
+            'recursive' => true,
+        ]);
+        $expectedFiles = FileHelper::findFiles(Yii::getAlias("@specs/issue_fix/14_module_config_in_url_prefixes/mysql"), [
+            'recursive' => true,
+        ]);
+        $this->checkFiles($actualFiles, $expectedFiles);
     }
 }
